5491195551efc33979d44b1d331b261c949c7664
Yerlan-Tleubekov <tterlan19@gmail.com>
2020-03-20 13:18:20 +0600

    feat: done typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  try {
+  let current = 0
+  array.map( item => {
+    if ( item < current ) {
+      current = item;
+    }
+  })
+  return current;
+  } catch (error) {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  try {
+    let current = 0
+    array.map( item => {
+      if ( item > current ) {
+        current = item;
+      }
+    })
+    return current;
+    
+  } catch (error) {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  console.log(typeof array);
+  try {
+    return array.length > 0 
+    ? array.reduce((prev, next) => prev + next, 0) / 20
+    : 0;
+    
+  } catch (error) {
+    return 0
+  }
+ 
+
 }
